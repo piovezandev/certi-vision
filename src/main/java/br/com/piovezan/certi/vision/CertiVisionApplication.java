f52df004087e60fea8eb8f756f1251f004a3fe79
Alan Piovezan <alan.log.piovezan@gmail.com>
2026-02-16 22:31:47 -0300

    Criando repositorio

---
+++ src/main/java/br/com/piovezan/certi/vision/CertiVisionApplication.java
@@ -0,0 +1,13 @@
+package br.com.piovezan.certi.vision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertiVisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertiVisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/piovezan/certi/vision/CertiVisionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.piovezan.certi.vision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertiVisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
